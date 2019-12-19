f611aaf4c4c2f8229e92d6312b610b331c3bc8f2
richardadinugraha <57470246+richardadinugraha@users.noreply.github.com>
2019-12-19 12:35:20 +0700

    Add files via upload

--- Soal_2.py
+++ Soal_2.py
@@ -4,14 +4,22 @@ import numpy as np
 dfProfesi = pd.read_csv('profesi.csv', delimiter = '|')
 
 # # # No 1
-# Profesi= dfProfesi['occupation'].unique()
-# jumlahProfesi= dfProfesi['occupation'].nunique()
-# print (f'jumlah profesi : {jumlahProfesi}')
-# print (Profesi)
+Profesi= dfProfesi['occupation'].unique()
+jumlahProfesi= dfProfesi['occupation'].nunique()
+print (f'jumlah profesi : {jumlahProfesi}')
+print (Profesi)
 
 
 # No 2
-ageTech=[]
-for i in dfProfesi.loc[['technician'], ['occupation']] :
-        age.append(dfProfesi.loc([i],['age']))
-print(age)
+dfHitung = dfProfesi.groupby([dfProfesi['occupation'],dfProfesi['gender']]).describe()
+dfHitung = dfHitung['age'][['max','min','mean']]
+dfHitung.rename(columns={'max':'max_usia','min':'min_usia','mean':'rerata_usia'},inplace=True)
+print (dfHitung)
+
+# No 3
+gender = pd.crosstab(dfProfesi.occupation, dfProfesi.gender).apply(lambda i: i/i.sum(), axis= 1) *100
+gender['%total'] = gender['F']+gender['M']
+gender.rename(columns={'F':'%female','M':'%male'},inplace=True)
+gender = gender[['%male','%female','%total']]
+print(gender)
+
